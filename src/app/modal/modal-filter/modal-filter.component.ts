71752523c26f79d675d283edecb04de05029ec28
anton <antonkuzmn1@gmail.com>
2024-05-25 22:19:43 +0700

    init modal-filter component

--- src/app/modal/modal-filter/modal-filter.component.ts
+++ src/app/modal/modal-filter/modal-filter.component.ts
@@ -4,10 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-modal-filter',
   standalone: true,
   imports: [],
-  template: `
-    <p>modal-filter works!</p>
-  `,
-  styleUrl: '../../../assets/styles/components/modal-filter.component.sass'
+  templateUrl: './modal-filter.component.html',
+  styleUrl: './modal-filter.component.sass'
 })
 export class ModalFilterComponent {
 
